368fbf00f4fcc64725f3156448e8686eb6e87f19
Ritvik <39237279+imritvik@users.noreply.github.com>
2019-10-31 16:11:13 +0530

    Update digit.asm

--- digit.asm
+++ digit.asm
@@ -5,7 +5,7 @@ section .text
 	global _start
 
 _start:
-	mov rax,7
+	mov rax,7	;this 7 is going to be printed you can change it to the number you want
 	call _printdigit
 
 	mov rax,60
@@ -14,8 +14,8 @@ _start:
 
 _printdigit:
 
-	add rax,48
-	mov [dig],al
+	add rax,48	;48 is ascii for 0 and 0+7 is 7
+	mov [dig],al	;changing the least significant byte of rax which is 0 from *dig
 	mov rax,1
 	mov rdi,1
 	mov rsi,dig
